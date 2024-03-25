0ec4e21433e8e6715eb55c184967c20b618fa86a
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-03-25 08:20:05 +0800

    Update

--- app/src/main/java/com/example/api/MainActivity.java
+++ app/src/main/java/com/example/api/MainActivity.java
@@ -15,7 +15,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvResult;
-    JsonPlaceholderAPI jsonPlaceholderAPI;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,76 +22,7 @@ public class MainActivity extends AppCompatActivity {
         tvResult = findViewById(R.id.tv_APIResult);
 
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://jsonplaceholder.typicode.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        jsonPlaceholderAPI = retrofit.create(JsonPlaceholderAPI.class);
-        getPost();
-//        getComments();
     }
 
-    private void getPost() {
 
-        // getting the response from the API
-        Call<List<Post>> call = jsonPlaceholderAPI.getPosts(4);
-
-        call.enqueue(new Callback<List<Post>>() {
-            @Override
-            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                if (!response.isSuccessful()){
-                    tvResult.setText("Code: " + response.code());
-                    return;
-                }
-                List<Post> posts = response.body();
-                for(Post post: posts){
-                    String content = "";
-                    content += "ID: " + post.getId() + "\n";
-                    content += "User ID: " + post.getUserId() + "\n";
-                    content += "Title: " + post.getTitle() + "\n";
-                    content += "Body: " + post.getText() + "\n\n";
-
-                    tvResult.append(content);
-
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Post>> call, Throwable t) {
-
-                tvResult.setText(t.getMessage());
-            }
-        });
-
-    }
-    private void getComments(){
-        Call<List<Comment>> call = jsonPlaceholderAPI.getComments(4);
-        call.enqueue(new Callback<List<Comment>>() {
-            @Override
-            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
-
-                if (!response.isSuccessful()){
-                    tvResult.setText(response.code());
-                    return;
-                }
-                List<Comment> comments = response.body();
-
-                for (Comment comment: comments){
-                    String content = "";
-                    content += "ID: " + comment.getId() + "\n";
-                    content += "Post ID: " + comment.getPostId() + "\n";
-                    content += "Name: " + comment.getName() + "\n";
-                    content += "Email: " + comment.getEmail() + "\n";
-                    content += "Body: " + comment.getText() + "\n\n";
-                    tvResult.append(content);
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Comment>> call, Throwable t) {
-                tvResult.setText(t.getMessage());
-            }
-        });
-    }
 }
